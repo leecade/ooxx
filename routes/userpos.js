89debc20ce1aad6b1e0b840fd5cd9651bdef1474
leecade <zhangxiaokun@BB0000021195BD.internal.baidu.com>
2013-03-24 09:03:54 +0800

    update files

--- routes/userpos.js
+++ routes/userpos.js
@@ -10,47 +10,40 @@ exports.getpos = function(req, res) {
 
    
    
-   var posKey  = "pos:"+uid;
+   var posKey  = "pos"+uid;
    var posVal  = x+","+y;
   //save user pos 
-  redis.set(posKey,posVal);
+  redis.hgetall(posKey,function(err, rep){
+	 rep['pos'] = posVal;
+	 console.log(rep);
+	 redis.hmset(posKey, rep);
+  });
+
+  
 
 
   //get user firends pos 
   actJoinKey = "active_join_"+ team + activeId;
-  redis.multi().smembers(actJoinKey).exec(function(err, rep){
-	var arr = rep[0];
-	var arrSize = rep[0].length;
+  redis.smembers(actJoinKey,function(err,rep){
 	
-	arr.forEach(function(val, i){
-	   redis.get(val, function(err, posVal){
-		
-		var tmpKey = "tmp" + team + activeId +"_"+ val+ "_" + posVal;
-
-	         redis.set(tmpKey, "1");
-	   });	
+	var multi = redis.multi();
+	rep.forEach(function(val){
+		multi.hgetall(val);
 	});
+	multi.exec(function(er,rul){
+		var posArr = {}; 
+		rul.forEach(function(val, j){
+			var xyArr = val['pos'].split(",");
+			posArr[j]    = {};
+			posArr[j]['x'] = xyArr[0];
+			posArr[j]['y'] = xyArr[1];
+			posArr[j]['uid'] = val['uid'];
+		});
 
-  });
-
-    redis.keys("tmp"+team+activeId + "*", function (err, rep){
-	var posArr = {};
+		var posStr = "";
+		posStr = JSON.stringify(posArr);
+		res.end(posStr);
+	});
+});
 	
-	rep.forEach(function(val, j){
-		var posInfoArr = val.split("_");
-		var userIdArr = posInfoArr[1].split(":");
-		var userId = userIdArr[1].toString();
-	    posArr[j] = {};
-		posArr[j]['pos'] = {};
-		posArr[j]['uid'] = {};
-	//	console.log(posInfoArr[2]);
-	        posArr[j]['pos'] = posInfoArr[2];
-			posArr[j]['uid'] = userId;
-	});
-	var posStr = JSON.stringify(posArr);
-
-	  
-	res.end(posStr);
-  });
-  
 } 
